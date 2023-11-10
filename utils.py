92a1b4fc8a5c57e604550dc6477a1f3f3acc22e6
Marina <nozdryakovamarina94@gmail.com>
2023-11-10 23:23:27 +0400

    get

--- utils.py
+++ utils.py
@@ -3,70 +3,63 @@ import time
 import shutil
 
 import requests
-from selenium.common import exceptions
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-def  make_folders(name: list) -> None:
-    if not os.path.isdir('dataset'):
-        os.mkdir('dataset')
-        
-    os.chdir('dataset')
-    if os.isdir(name[0]) and os.isdir(name[1]):
-        shutil.rmtree(name[0])
-        shutil.rmtree(name[1])
-    os.mkdir(name[0])
-    os.mkdir(name[1])
+def make_folder(name: str) -> None:
+    if not os.path.isdir(name):
+        os.mkdir(name)
 
 
-def get_link(request):
-    service = ChromeService(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    URL = f"https://yandex.ru/images/search?text={request}"
-    driver.get(URL=URL)
-    driver.maximize_window()
-    time.sleep(3)
-    driver.find_element(By.CSS_SELECTOR, 'div.serp-item__preview a.serp-item__link').click()
-
-    with open(f"urls_{request}.txt", 'w') as file:
-        for i in range(0, 1500):
-            try:
-                time.sleep(0.05)
-                link = driver.find_element(By.CSS_SELECTOR, "a.Button2_view_action").get_attribute("href")
-                file.write(link + '\n')
-                driver.find_element(By.CSS_SELECTOR, "div.CircleButton:nth-child(4)").click()
-            except:
-                continue
+def get_img(name: str, num_img: int) -> list:
+    url = F'https://yandex.ru/images/search?text={name}'
+    driver = webdriver.Edge()
+    driver.get(url=url)
+    driver.find_element(By.CSS_SELECTOR, 'a.Link').click()
+    item = driver.find_element(By.CLASS_NAME, 'MMImage-Origin')
+    items = item.get_attribute('src')
+    links = [items]
+    for i in range(num_img):
+        try:
+            time.sleep(0.5)
+            driver.find_element(By.CLASS_NAME, 'MediaViewer-ButtonNext').click()
+            img = driver.find_element(By.CLASS_NAME, 'MMImage-Origin')
+            img_l = img.get_attribute('src')
+        except:
+            continue
     driver.quit()
+    return links
 
 
-def get_image(name: str) -> None:
-    os.chdir(name)
-
-    count = 0
-    with open(f"urls_{name}.txt", "r") as file:
-        for line in file:
-            try:
-                url = line.strip()
-                time.sleep(2)
-                response = requests.get(url, stream=True)
-                if response.status_code == 200:
-                    count+=1
-                    with open(f"dataset/{name}/{str(count).zfill(4)}.jpg", "wb") as image_file:
-                        shutil.copyfileobj(response.raw, image_file)
-                else:
+def upload_img(name: str, links: list) -> None:
+    make_folder(F'dataset/{name}')
+    num_img = 0
+    for img_l in links:
+            while True:
+                try:
+                    time.sleep(3)
+                    response = requests.get(img_l, verify=True)
+                    if response.status_code == 200:
+                        num_img += 1
+                        with open(str(num_img).zfill(4) +'jpg', 'wb') as f_img:
+                            f_img.write(response.content)
+                            print('Excellent')
+                            break
+                except:
                     continue
-            except:
-                continue
-    print(f'{count} Succecs!')
-    
+
 
 def main() -> None:
-    type1 = 'polarbear'
-    type2 = 'brownbear'
-    make_folders((type1, type2))
-    get_image(type1)
-    get_image(type2)
+    if os.path.isdir("dataset"):
+        shutil.rmtree("dataset")
+    
+    num_img = 10
+
+    name1 = 'polarbear'
+    upload_img(name1, get_img(name1, num_img))
+
+    name2 = 'brownbear' 
+    upload_img(name2, get_img(name2, num_img))
